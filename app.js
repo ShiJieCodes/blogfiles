b4d828f32ed72a0493f6421444d96db41cc97220
Lim Shi Jie <shijielim@gmail.com>
2023-07-30 15:18:38 +0800

    Almost done but site is not mobile responsive yet. Still alot of clean up to do and my code is not neat yet

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// drop down for modal start
 const modals = document.querySelectorAll(".modal");
 
 modals.forEach(function (modal) {
@@ -8,6 +9,54 @@ modals.forEach(function (modal) {
         item.classList.remove("show-text");
       }
       modal.classList.toggle("show-text");
+    });
+  });
+});
+// drop down for modal end
+
+// fixed navbar start
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset;
+  const navbarHeight = navbar.getBoundingClientRect().height;
+
+  if (scrollHeight > navbarHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+
+  const topLink = document.querySelector(".top-link");
+  if (scrollHeight > 800) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
+
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    // prevent default
+    console.log("hey");
+    e.preventDefault();
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    // calculate the heights
+    const navHeight = navbar.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains("fixed-nav");
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position -= navHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
     });
   });
 });
